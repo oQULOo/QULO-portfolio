85c03516f94f737bd1ce41a0c98a5ee363181418
oQULOo <qulo.ooo.my@gmail.com>
2021-04-17 12:49:26 +0900

    masterブランチへの初期設定

---
+++ .cache/sync-requires.js
@@ -0,0 +1,15 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\CHLO\\qulo-portfolio\\qulo-portfolio-gatsby\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\CHLO\\qulo-portfolio\\qulo-portfolio-gatsby\\src\\pages\\404.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("C:\\Users\\CHLO\\qulo-portfolio\\qulo-portfolio-gatsby\\src\\pages\\about.js"))),
+  "component---src-pages-elements-js": hot(preferDefault(require("C:\\Users\\CHLO\\qulo-portfolio\\qulo-portfolio-gatsby\\src\\pages\\elements.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\CHLO\\qulo-portfolio\\qulo-portfolio-gatsby\\src\\pages\\index.js"))),
+  "component---src-templates-blog-post-js": hot(preferDefault(require("C:\\Users\\CHLO\\qulo-portfolio\\qulo-portfolio-gatsby\\src\\templates\\blog-post.js")))
+}
+
